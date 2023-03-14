d7f48c40523c4e318a79c5b5d187f3dee3623137
teddy wambua <teddywambua5456@gmail.com>
2023-03-14 13:19:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+    const input = document.getElementById("button");
+    function clickAlert() {
+        alert("Clicked!");
+    }
+    input.addEventListener("click", clickAlert);
+}
+addingEventListener();
